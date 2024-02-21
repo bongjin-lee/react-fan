b29e8d3d0920c279e27fdabb4df98f41c98ef63b
bongjin-lee <you@example.com>
2024-02-21 17:55:59 +0900

    clone

---
+++ src/shared/Router.jsx
@@ -0,0 +1,29 @@
+import Detail from "pages/Detail";
+import Home from "pages/Home";
+import Login from "pages/Login";
+import Profile from "pages/Profile";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+export default function Router() {
+  const [fanLogin, setFanLogin] = useState(false);
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+       {fanLogin ? (
+       <>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<Navigate replace to="/" />} /> </> 
+        ) : (
+        <>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate replace to="/login" />} />
+        </>
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
+}
