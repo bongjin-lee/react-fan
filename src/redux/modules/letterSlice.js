5ba15313b0307caa274fdc649dbaf3ece45772a5
bongjin-lee <you@example.com>
2024-02-21 18:16:24 +0900

    feat: member, letter 리팩토링

--- src/redux/modules/letterSlice.js
+++ src/redux/modules/letterSlice.js
@@ -1,32 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
 import fakeData from "fakeData.json";
-// 팬레터 추가
-const ADD_LETTER = "letters/ADD_LETTER";
-// 팬레터 삭제
-const DELETE_LETTER = "letters/DELETE_LETTER";
-// 팬레터 수정
-const EDIT_LETTER = "letters/EDIT_LETTER";
-
-export const addLetter = (payload) => {
-  return { type: ADD_LETTER, payload };
-};
-export const deleteLetter = (payload) => {
-  return { type: DELETE_LETTER, payload };
-};
-export const editLetter = (payload) => {
-  return { type: EDIT_LETTER, payload };
-};
 
 const initialState = fakeData;
 
-const letters = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_LETTER:
+const letterSlice = createSlice({
+  name: "letter",
+  initialState,
+  reducers: {
+    addLetter: (state, action) => {
       const newLetter = action.payload;
-      return [newLetter, ...state];
-    case DELETE_LETTER:
+      state.unshift(newLetter);
+    },
+    deleteLetter: (state, action) => {
       const letterId = action.payload;
       return state.filter((letter) => letter.id !== letterId);
-    case EDIT_LETTER:
+    },
+    editLetter: (state, action) => {
       const { id, editingText } = action.payload;
       return state.map((letter) => {
         if (letter.id === id) {
@@ -34,9 +23,9 @@ const letters = (state = initialState, action) => {
         }
         return letter;
       });
-    default:
-      return state;
-  }
-};
+    },
+  },
+});
 
-export default letters;
+export const { addLetter, deleteLetter, editLetter } = letterSlice.actions;
+export default letterSlice.reducer;
